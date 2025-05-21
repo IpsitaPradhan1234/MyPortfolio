b41e9a813d73aabf357ca587a252c18879072fb0
AW-ipsitapradhan <ipsitapradhan@aviraworld.co.in>
2025-05-22 00:22:06 +0530

    ipsita's portfolio

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Final.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/assets/IpsitaPradhan.mp4",
+    },
+];
